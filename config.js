2acbec66f8fca476f7c879eae71908723c7db681
kamileecher <58888200+kamileecher@users.noreply.github.com>
2021-04-21 18:41:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -12,13 +12,13 @@ const PARALLEL_LIMIT = 20 // The number of parallel network requests can be adju
 const DEFAULT_TARGET = '' // Required. Copy the default destination ID. If target is not specified, it will be copied here. It is recommended to fill in the team drive ID
 
 const AUTH = { // If you have the json authorization file of the service account, you can copy it to the sa directory instead of client_id/secret/refrest_token
-  client_id: 'your_client_id',
-  client_secret: 'your_client_secret',
-  refresh_token: 'your_refrest_token',
-  expires: 0, // Can be left blank
-  access_token: '', // Can be left blank
-  tg_token: 'bot_token', // Your telegram bot token，Go here https://core.telegram.org/bots#6-botfather
-  tg_whitelist: ['your_tg_username'] // Your tg username(t.me/username)，The bot will only execute commands sent by users in this list
+  client_id: '714410736546-1ao1aunvau1jd76haschnpntjivcgtg1.apps.googleusercontent.com',
+  client_secret: 'Tp1CDBNelvImgV2kxFP8yub5',
+  refresh_token: '1//09v8BJXRujK3PCgYIARAAGAkSNgF-L9Ir9MwiL2ZG0mWEE35014rFXszvUjupZCOwRApEAMvR8Pz2wyq8BRN9Jhlfb_Ab7HS8bg',
+  expires: "2021-03-19T20:30:01.0904672+03:00", // Can be left blank
+  access_token: 'ya29.a0AfH6SMCzMJ8VTLWkmJmQhqoZCkEq7_4-ifvVuRGXzbm4dcDNuxQ1mak3fDhaQyyXMb1nwEMQAlhShO1fE4cWNbtwei795KCQe-J4ANWbkjCWKFr6D3JY5_1lPDXeBPOdEZo1GakYIzLD3XnZ7YYbdwUSWAytr3A', // Can be left blank
+  tg_token: '1732876956:AAET-imAdejNGh1T03tvRdRPNFXwlR0qZS8', // Your telegram bot token，Go here https://core.telegram.org/bots#6-botfather
+  tg_whitelist: ['t.me/kamileecher'] // Your tg username(t.me/username)，The bot will only execute commands sent by users in this list
 }
 
 module.exports = { AUTH, PARALLEL_LIMIT, RETRY_LIMIT, TIMEOUT_BASE, TIMEOUT_MAX, LOG_DELAY, PAGE_SIZE, DEFAULT_TARGET }
